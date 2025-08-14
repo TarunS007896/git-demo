9564f77d3ae7fbc816d1fc2b26bd82cba5ce7ce9
TarunS007896 <tarunshankar64@gmail.com>
2025-08-14 16:20:22 +0530

    added  1 line

--- demo.py
+++ demo.py
@@ -2,5 +2,5 @@ print('I love python')
 print('added line')
 print('added line')
 print("I don't want to upload this change")
-print('added one line')
+print('added 1 line')
 
--- stage.py
+++ stage.py
@@ -1,3 +1,4 @@
 print('created one file')
 print("i want to upload this change")
 print('added one line')
+print('added 1 line')
